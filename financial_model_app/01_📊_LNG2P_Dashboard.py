802ba4c844c9d2d6e08e402e5db82a76977e8850
Motlatsi Seotsanyana <meotsanyana@gmail.com>
2025-05-09 12:13:44 +0200

    Update an import path.

--- financial_model_app/01_📊_LNG2P_Dashboard.py
+++ financial_model_app/01_📊_LNG2P_Dashboard.py
@@ -2,10 +2,9 @@ import os
 
 import pandas as pd
 import streamlit as st
-
-from financial_model_app.src.components.dashboard import show_dashboard_charts
-from financial_model_app.src.components.sidebars import show_dashboard_sidebar
-from financial_model_app.src.utils.load_data import (
+from src.components.dashboard import show_dashboard_charts
+from src.components.sidebars import show_dashboard_sidebar
+from src.utils.load_data import (
     load_emission_factors_data,
     load_plant_data,
     load_scenario_data,
